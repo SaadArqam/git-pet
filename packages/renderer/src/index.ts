2a99415d72350630843674206c1a5f7f90b5ed2a
Saad Arqam <saadarqamrnc@gmail.com>
2026-03-26 09:51:16 +0530

    fix: remove invalid turntableView export from renderer

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,4 +1,4 @@
 export const version = '0.0.1';
-export { drawPet, turntableView } from "./draw";
+export { drawPet} from "./draw";
 export { getSprite, getSpriteView } from "./sprites";
 export type { Pixel, SpriteView, PetView } from "./sprites";
