430674cdbb63a529ffd14821a6049dd523d732e7
Saad Arqam <saadarqamrnc@gmail.com>
2026-03-28 21:15:22 +0530

    fix: vercel build errors in apps/web/app/page.tsx.

--- apps/web/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-export default function Page() {
-  return <ClientComp />;
-}
-function ClientComp() {
-  "use client";
-  useEffect(() => {}, []);
-  return <div>Client</div>;
-}
